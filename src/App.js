5ec61ee968f2b2b211320bae586f6c93ad910ee8
Tanner Alsup <tanneralsup14@gmail.com>
2020-02-29 17:51:55 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GridItem = (props) => (
+  <div className="grid__glex">
+    <img className="grid__img" src={props.image} />
+  </div>
+)
+
+class App extends Component {
+  
+  state = {
+    error: null,
+    isLoaded: false,
+    items: []
+  }
+
+  componentDidMount() {
+    const query = `
+    query {
+      Page {
+        media( sort: POPULARITY_DESC) {
+          genres
+          isAdult
+          averageScore
+          id
+          title {
+            english
+          }
+          coverImage {
+            large
+          }
+        }
+      }
+    }
+    `;
+
+    const variables = {};
+
+    this.getAnime(query, variables)
+
+  }
+  
+  getAnime = async (query, variables) => {
+    try {
+      const response = await axios.post('https://graphql.anilist.co', {
+        query,
+        variables
+      });
+
+      console.log(response.data)
+
+      this.setState(() => ({
+        isLoaded: true,
+        items: response.data.data.Page.media
+      }));
+
+    } catch (error) {
+      this.setState(() => ({ error }))
+    }
+  }
+  render() {
+
+    const { error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <div>{error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+
+      return (
+        <div className="grid">
+          {items.map(item => (
+            <GridItem key={item.id} image={item.coverImage.large} />
+          ))}
+        </div>
+      )
+    }
+  }
 }
+
+
+
+
+
+  
+
+
 
 export default App;
